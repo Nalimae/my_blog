dabc320580e8f179619dac9324f66a663b0e9d53
Naftali <nalimaemurugu@gmail.com>
2023-02-10 23:45:40 +0300

    my blog

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.http import Http404
+from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 # Create your views here.
+
+"""
 def post_list(request):
 	post_list = Post.published.all()
 	#pagination with 3 posts per page
@@ -17,8 +23,56 @@ def post_list(request):
 		# If page_number is out of range deliver last page of results
 		posts = paginator.page(paginator.num_pages)
 	return render(request,'blog/post/list.html',{'posts':posts})
+"""
+@require_POST
+def post_comment(request, post_id):
+	post=get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+	comment = None
+	#A comment was posted
+	form = CommentForm(data=request.POST)
+	if form.is_valid():
+		# Create a comment object without saving it to the database
+		comment = form.save(commit=False)
+		#assign the post to the comment
+		comment.post = post
+		#save the comment to the database
+		comment.save()
+	return render(request, 'blog/post/comment.html',{'post':post,'form':form,'comment':comment})
 
 def post_detail(request,year, month,day, post):
 	post = get_object_or_404(Post,status=Post.Status.PUBLISHED,slug=post,publish__year=year,publish__month=month,publish__day=day)
-	return render(request,'blog/post/detail.html',{'post':post})
+#list of active comments for this post
+	comments = post.comments.filter(active=True)
+	#form for users to comment
+	form = CommentForm()
+	return render(request,'blog/post/detail.html',{'post':post,'comments':comments,'form':form})
 	
+
+#Using class based views
+
+class PostListView(ListView):
+	queryset = Post.published.all()
+	context_object_name= 'posts'
+	paginate_by=3
+	template_name='blog/post/list.html'
+
+def post_share(request,post_id):
+	#retrieve post by id
+	post = get_object_or_404(Post, id=post_id,status=Post.Status.PUBLISHED)
+	sent= False
+	if request.method == 'POST':
+		#FORM WAS SUBMITTED
+		form = EmailPostForm(request.POST)
+		if form.is_valid():
+			#form fields passed validation
+			cd= form.cleaned_data
+			post_url = request.build_absolute_uri(post.get_absolute_url())
+			subject = f"{cd['name']} recommends you read" f"{post.title}"
+			message = f"Read{post.title} at {post_url}\n\n"
+			f"{cd['name']}\'s comments: {cd['comments']}"
+			send_mail(subject, message, 'nalimaemurugu@gmail.com',[cd['to']])
+			sent=True
+
+	else:
+		form = EmailPostForm()
+	return render(request,'blog/post/share.html',{'post':post,'form':form,'sent':sent})
